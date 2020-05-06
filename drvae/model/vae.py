a332c3cb05eda047cfb2ecd206fabd1b47681e1b
Dan Biderman <danbider@gmail.com>
2020-05-06 17:58:39 -0400

    minor debug

--- drvae/model/vae.py
+++ drvae/model/vae.py
@@ -215,13 +215,11 @@ class ConvDRVAE(ConvVAE):
 
         # push data and recond through discrim_model
         # ToDo - validate that this works.
-        #zdiscrim_data  = self.discrim_model[0](data)#[:, self.dim_out_to_use]
-        #zdiscrim_recon = self.discrim_model[0](recon_data)#[:, self.dim_out_to_use]
-        zdiscrim_data  = self.discrim_model(data)#[:, self.dim_out_to_use]
-        zdiscrim_recon = self.discrim_model(recon_data)#[:, self.dim_out_to_use]
+        zdiscrim_data  = self.discrim_model[0](data)#[:, self.dim_out_to_use]
+        zdiscrim_recon = self.discrim_model[0](recon_data)#[:, self.dim_out_to_use]
+        #zdiscrim_data  = self.discrim_model(data)#[:, self.dim_out_to_use]
+        #zdiscrim_recon = self.discrim_model(recon_data)#[:, self.dim_out_to_use]
 
-        print(zdiscrim_data.shape)
-        print(zdiscrim_recon.shape)
         # zdiscrim_data  = self.discrim_model[0](
         #     data.view(data.shape[0],-1))#[:, self.dim_out_to_use]
         # zdiscrim_recon = self.discrim_model[0](
@@ -230,7 +228,6 @@ class ConvDRVAE(ConvVAE):
         disc_loss = self.discrim_beta * \
             torch.sum((zdiscrim_data
                        -zdiscrim_recon)**2)
-        print(disc_loss)
         assert ~np.isnan(vae_loss.clone().detach().cpu())
         assert ~np.isnan(disc_loss.clone().detach().cpu())
         return vae_loss + disc_loss
